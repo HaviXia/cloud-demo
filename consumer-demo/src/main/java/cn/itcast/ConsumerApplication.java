19fe7885b457de487d2471d3e1e400c0ddb3ee41
hkxia95 <hongkangxia@163.com>
2021-02-10 18:15:27 +0800

    # 服务之间的远程调用 # 两个tomcat之间的交互 # consumer->user-service->DB # 动态获取eureka中服务列表，服务ip和端口号 # 高可用Eureka，Eureka集群相互注册 # Ribbon 依赖，实现负载均衡,配置文件配置负载均衡算法

--- consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
+++ consumer-demo/src/main/java/cn/itcast/ConsumerApplication.java
@@ -2,6 +2,7 @@ package cn.itcast;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -17,6 +18,7 @@ import org.springframework.web.client.RestTemplate;
 public class ConsumerApplication {
     //注册一个restTemplate
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
--- consumer-demo/src/main/java/cn/itcast/consumer/web/ConsumerController.java
+++ consumer-demo/src/main/java/cn/itcast/consumer/web/ConsumerController.java
@@ -5,6 +5,7 @@ import cn.itcast.consumer.pojo.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.netflix.ribbon.RibbonLoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,7 +29,8 @@ public class ConsumerController {
 
     //动态获取eureka中服务
     @Autowired
-    private DiscoveryClient discoveryClient;
+    //private DiscoveryClient discoveryClient;
+    private RibbonLoadBalancerClient ribbonLoadBalancerClient; //添加 Ribbon 实现负载均衡
 
     /*
      * 掉用的另外的一个 tomcat 也就是 user-service 对应的 tomcat
@@ -42,12 +44,20 @@ public class ConsumerController {
          * */
 
         // 根据服务id获取实例
-        List<ServiceInstance> instances = discoveryClient.getInstances("user-service");//参数是服务id
-        //从实例中取出ip和端口
-        ServiceInstance serviceInstance = instances.get(0);//0号,将来需要负载均衡算法，随机选Instances
-        //取出端口
-        String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
+        // List<ServiceInstance> instances = discoveryClient.getInstances("user-service");//参数是服务id
+        // 从实例中取出ip和端口
+        // ServiceInstance serviceInstance = instances.get(0);//0号,将来需要负载均衡算法，随机选Instances
+        // 取出端口
+        // String url = "http://" + serviceInstance.getHost() + ":" + serviceInstance.getPort() + "/user/" + id;
 
+        /*
+         * 使用 Ribbon 做负载均衡
+         * 找到启动类,添加 LoadBalanced注解，添加到 restTemplate 身上，loadBalanced内置拦截器拦截 restTemplate请求
+         *
+         * */
+        //ServiceInstance choose = ribbonLoadBalancerClient.choose("user-service"); //得到一个实例，默认轮询
+
+        String url = "http://user-service/user/" + id;
         User user = restTemplatere.getForObject(url, User.class);
         return user;
     }
