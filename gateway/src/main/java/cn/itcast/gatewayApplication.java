37df45c7acabf0a388b355b295656532063179d0
hkxia95 <hongkangxia@163.com>
2021-02-15 02:01:25 +0800

    # 服务之间的远程调用 # 两个tomcat之间的交互 # consumer->user-service->DB # 动态获取eureka中服务列表，服务ip和端口号 # 高可用Eureka，Eureka集群相互注册 # Ribbon 依赖，实现负载均衡,配置文件配置负载均衡算法 # 消费类Controller上添加服务降级（Hystrix） # yml文件中配置对全局/服务/方法的默认超时时长 # Hystrix 服务熔断，三个状态机、默认失败次数、错误率 # Feign服务调用，UserClient传入请求四个条件 # Feign的服务熔断、请求压缩 # zuul使用eureka实现面向服务的路由

---
+++ gateway/src/main/java/cn/itcast/gatewayApplication.java
@@ -0,0 +1,20 @@
+package cn.itcast;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+
+/**
+ * @author ：hkxia
+ * @description：TODO 网关应用
+ * @date ：2021/2/15 01:49
+ */
+
+@EnableZuulProxy
+@SpringBootApplication
+public class gatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(gatewayApplication.class);
+    }
+}
